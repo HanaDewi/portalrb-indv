ea5c4edf6a0035fc723605bbd3e8a4425d956ef8
oki.menpanrb <oki.menpanrb@gmail.com>
2023-08-06 17:32:59 +0700

    add default Value Null to user.foto to make seeder work

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('level');
-            $table->string('foto');
+            $table->string('foto')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
