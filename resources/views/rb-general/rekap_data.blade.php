948a10633f25a64b90d4a1facd2fb0ea7c970954
Abdul Azis <140942821+abdulazis483@users.noreply.github.com>
2023-08-15 15:16:41 +0700

    join column tahun target

--- resources/views/rb-general/rekap_data.blade.php
+++ resources/views/rb-general/rekap_data.blade.php
@@ -139,7 +139,7 @@
                 'scrollX': true,
                 'orderFixed': [0, 'asc'],
                 'autoWidth': false,
-                'rowsGroup': [1, 2, 3, 4, 5, 6],
+                'rowsGroup': [1, 2, 3, 4, 5, 6,7],
             });
         });
     </script>
