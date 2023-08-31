f5536d726c849c9b35524915aae36d2d691e7e60
Abdul Azis <140942821+abdulazis483@users.noreply.github.com>
2023-08-31 12:51:17 +0700

    update home banner

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
             <div class="box">
                 <div class="p-5">
                     <div class="rounded-md">
-                        <img width="100%" alt="menpanrb" class="rounded-md" src="{{ asset('template_lkerb') }}/dist/images/bannerpanrb.jpg">
+                        <img width="100%" alt="menpanrb" class="rounded-md" src="{{ asset('template_lkerb') }}/dist/images/bannerportalpanrb.jpg">
                     </div>
                 </div>
             </div>
