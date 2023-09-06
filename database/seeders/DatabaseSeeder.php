ce38b52f9701e4ce7f5ad15b721de56ee01622d5
oki.menpanrb <oki.menpanrb@gmail.com>
2023-09-06 10:57:38 +0700

    add tema seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,8 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             KegiatanUtamaSeeder::class,
             IndikatorSeeder::class,
+            TemaSeeder::class,
+
         ]);
     }
 }
