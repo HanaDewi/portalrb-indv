41a83cc9d0c3d39c353b6521e4db65ce3e79eb91
Oki Priyadi <oki.menpanrb@gmail.com>
2024-04-14 10:34:58 +0700

    update TematikRencanaAksiModel

--- app/Models/TematikRencanaAksi.php
+++ app/Models/TematikRencanaAksi.php
@@ -9,4 +9,8 @@ class TematikRencanaAksi extends Model
 {
     use HasFactory;
     protected $table = 'tematik_rencana_aksi';
+    public function output()
+    {
+        return $this->hasMany(TematikRencanaAksiOutput::class, 'tematik_rencana_aksi_id');
+    }
 }
