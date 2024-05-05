aa46b7e84fde051094567a0705a139361830a65a
yopi <yopi@tura.group>
2024-05-05 17:39:18 +0700

    fix: tag error

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -3,12 +3,7 @@
 
 @section('content')
 @include('common.status')
-<<<<<<< HEAD
-<div class="intro-y col-span-12 lg:col-span-12">
-<form action="{{ url('profil_simpan') }}" id="form-user" method="post">
-=======
 <form action="{{ url('profil_simpan') }}" id="form-user" method="post" class="intro-y col-span-12 lg:col-span-12">
->>>>>>> 359d8c9 (fix: class style)
     @csrf
     <div class="grid grid-cols-6 gap-6">
         <div class="intro-y col-span-12 lg:col-span-6">
