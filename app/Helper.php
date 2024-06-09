ad261e73f4829f5d6939ea894099a29dfca3f5cc
Azkal Fikri <saklu07@gmail.com>
2024-06-09 08:16:30 +0700

    fix format number

--- app/Helper.php
+++ app/Helper.php
@@ -278,6 +278,13 @@ if(! function_exists('fnumber')) {
     }
 }
 
+if(! function_exists('fnumber2')) {
+    function fnumber2($number, $digit = 0)
+    {
+        return number_format($number, $digit, ',', '.');
+    }
+}
+
 if(! function_exists('instansis')) {
     function instansis()
     {
