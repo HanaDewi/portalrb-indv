6c0eeb9251a74386bdbb734c430955471e5aeba4
Azkal Fikri <saklu07@gmail.com>
2024-07-09 22:10:42 +0700

    fix file preview

--- app/Http/Controllers/DokumenController.php
+++ app/Http/Controllers/DokumenController.php
@@ -64,8 +64,10 @@ class DokumenController extends Controller
             $dokumen->filenya = '';
             foreach ($dokumen->files as $file) {
                 $ext = pathinfo($file->file, PATHINFO_EXTENSION);
-                $src = asset('storage/dokumen/' . $file->file);
-                $dokumen->filenya .= '<a href="' . $src . '" target="_blank" title="' . $file->deskripsi . '" class="inline-block"><img src="' . asset('images') . '/'.exts($ext).'" style="width: 50px; margin-right: 5px; margin-top: 5px;"></a>';
+                if (in_array($ext, exts())) {
+                    $src = asset('storage/dokumen/' . $file->file);
+                    $dokumen->filenya .= '<a href="' . $src . '" target="_blank" title="' . $file->deskripsi . '" class="inline-block"><img src="' . asset('images') . '/'.exts($ext).'" style="width: 50px; margin-right: 5px; margin-top: 5px;"></a>';
+                }
             }
         }
         return response()->json(['data' => $dokumens]);
