e8f70bece8fdf964ee362c0d83579c016ed3e17a
Oki Priyadi <oki.menpanrb@gmail.com>
2024-07-17 23:08:03 +0700

    disable generate skor

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/zi', [PengusulanZIController::class, 'index'])->name('pengusulan_zi');
     Route::post('/zi', [PengusulanZIController::class, 'store_bukti_dukung'])->name('pengusulan_zi_store');
     Route::get('/zi-tinjau', [PengusulanZIController::class, 'tinjau'])->name('tinjau_zi');
-    Route::get('/zi/generate_skor', [PengusulanZIController::class, 'generate_rekap_instansi_skor'])->name('pengusulan_zi');
+    #Route::get('/zi/generate_skor', [PengusulanZIController::class, 'generate_rekap_instansi_skor'])->name('pengusulan_zi');
     Route::get('/zi/admin', [PengusulanZIController::class, 'dashboard'])->name('dashboard_zi');
     Route::get('/zi/rekap-pengusulan', [PengusulanZIController::class, 'rekap_pengusulan'])->name('rekap_pengusulan');
     Route::get('/zi/update-predikat', [PengusulanZIController::class, 'update_predikat'])->name('update_predikat');
