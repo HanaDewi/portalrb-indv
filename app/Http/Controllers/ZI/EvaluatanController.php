79cc5365cd5bf036f8be009edc520fbf21fc46a2
Oki Priyadi <oki.menpanrb@gmail.com>
2024-08-23 14:15:31 +0700

    evaluatan belum bisa lihat seleksi administrasi

--- app/Http/Controllers/ZI/EvaluatanController.php
+++ app/Http/Controllers/ZI/EvaluatanController.php
@@ -37,7 +37,9 @@ class EvaluatanController extends Controller
             $status_akhir = $instansiZI->status_akhir;
             $unit_wbks = UnitZI::where("instansi_zi_id", $instansiZI->id)->where('wbk',1)->get();
             $unit_wbbms = UnitZI::where("instansi_zi_id", $instansiZI->id)->where('wbbm',1)->get();
-            return view('zi.evaluatan.seleksi_administrasi_zi', compact(
+            //return view('zi.evaluatan.seleksi_administrasi_zi', compact(
+            return view('zi.evaluatan.seleksi_desk_zi', compact(
+                
                  'instansi_id', 'instansi', 'group_kld', 'instansiZI',
                 'unit_wbks', 'unit_wbbms',
                 'syarat_akhir_wbk','syarat_akhir_wbbm', 'status_akhir'
