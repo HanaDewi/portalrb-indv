1efa2776bceba7eaac8b223caa374eaebeb0ad89
Oki Priyadi <oki.menpanrb@gmail.com>
2024-08-26 09:58:58 +0700

    fixed logic error

--- app/Http/Controllers/ZI/AdministrasiController.php
+++ app/Http/Controllers/ZI/AdministrasiController.php
@@ -94,13 +94,12 @@ class AdministrasiController extends Controller
                             $progress_teams[$tim]["jumlah_wbk"] += $wbkCount ;
                             $progress_teams[$tim]["jumlah_wbbm"] += $wbbmCount ;
                             if($wbbmCompletedCount>0 || $wbkCompletedCount>0 ){
-                                $jumlah_instansi_lolos += 1;
                                 $progress_teams[$tim]["jumlah_instansi_lulus"] +=1;
                                 if($wbbmCompletedCount>0 ) {
-                                    $progress_teams[$tim]["jumlah_wbk_final_total"] += $wbkCompletedCount ;
+                                    $progress_teams[$tim]["jumlah_wbk_final_total"] += $wbkFinalCount ;
                                 }
                                 elseif($wbkCompletedCount>0 ){  
-                                    $progress_teams[$tim]["jumlah_wbbm_final_total"] += $wbbmCompletedCount;
+                                    $progress_teams[$tim]["jumlah_wbbm_final_total"] += $wbbmFinalCount;
                                 }      
                             }
                         }
