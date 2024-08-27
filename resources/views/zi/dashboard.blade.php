31582ffcac8283129343b4a15fa0e4047b56dd41
Oki Priyadi <oki.menpanrb@gmail.com>
2024-08-27 09:15:18 +0700

    add menu

--- resources/views/zi/dashboard.blade.php
+++ resources/views/zi/dashboard.blade.php
@@ -19,7 +19,7 @@
             <a href="{{ route('rekap_pengusulan') }}">
                 <div class="flex items-center">
                     <div class="w-2/4 flex-none">
-                        <div class="text-lg font-bold truncate">Rekap Pengusualan ZI</div>
+                        <div class="text-lg font-bold truncate">Rekap Pengusulan ZI</div>
                         <div class="text-slate-500 mt-1">Rekap Pengusulan ZI setiap instansi</div>
                     </div>
                     <div class="flex-none ml-auto relative">
@@ -43,6 +43,36 @@
             </a>
         </div>
     </div>
+    <div class="col-span-12 sm:col-span-6 2xl:col-span-3 intro-y">
+        <div class="box p-5 zoom-in">
+            <a href="{{ route('seleksi_administrasi') }}">
+                <div class="flex items-center">
+                    <div class="w-2/4 flex-none">
+                        <div class="text-lg font-bold truncate">Seleksi Administrasi</div>
+                        <div class="text-slate-500 mt-1">Seleksi untuk mengecek kelengkapan administrasi</div>
+                    </div>
+                    <div class="flex-none ml-auto relative">
+                        <div class="w-[90px] h-[90px]">
+                            <canvas id="report-donut-chart-2" width="90" height="90"
+                                style="display: block; box-sizing: border-box; height: 90px; width: 90px;"></canvas>
+                        </div>
+                        <div class="font-medium absolute w-full h-full flex items-center justify-center top-0 left-0">
+                            <span><svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" viewBox="0 0 24 24"
+                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round" class="lucide lucide-file-bar-chart">
+                                    <path d="M14.5 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7.5L14.5 2z" />
+                                    <polyline points="14 2 14 8 20 8" />
+                                    <path d="M12 18v-4" />
+                                    <path d="M8 18v-2" />
+                                    <path d="M16 18v-6" />
+                                </svg> </span>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+
     <div class="col-span-12 sm:col-span-6 2xl:col-span-3 intro-y">
         <div class="box p-5 zoom-in">
             <a href="{{ route('logout') }}" onclick="event.preventDefault(); $('#logout').submit();">
