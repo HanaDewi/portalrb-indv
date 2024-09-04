2d2dd0eb1a0932cf4572a10c287e0efe83b60476
Oki Priyadi <oki.menpanrb@gmail.com>
2024-09-04 09:15:49 +0700

    update

--- resources/views/zi/template_lke_evaluator.blade.php
+++ resources/views/zi/template_lke_evaluator.blade.php
@@ -1,12 +1,12 @@
 @extends('zi.admin.rubick')
-@section('title', 'Rekap Data Pengusulan ZI - ' .auth()->user()->nama)
+@section('title', $title . ' - ' .auth()->user()->nama)
 
 @section('content')
 <div class="intro-y col-span-12 lg:col-span-12">
     @include('common.status')
     <div class="intro-y box">
         <div class="flex flex-col sm:flex-row items-center p-5 border-b border-slate-200/60">
-            <h2 class="font-bold text-base mr-auto"> Rekap Unit </h2>
+            <h2 class="font-bold text-base mr-auto"> {{$title}} </h2>
         </div>
         <br />
         <div class="col-span-12 grid grid-cols-12 " style="text-align:center;">
