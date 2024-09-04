a72774612fadb0309ca16b8e7fa3af426fa5000e
Oki Priyadi <oki.menpanrb@gmail.com>
2024-09-04 15:36:47 +0700

    log sanggah instansi

--- app/Models/ZI/SanggahUnit.php
+++ app/Models/ZI/SanggahUnit.php
@@ -2,19 +2,29 @@
 
 namespace App\Models\ZI;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Activitylog\LogOptions;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 
 class SanggahUnit extends Model
 {
     use HasFactory;
+    use LogsActivity;
     protected $connection = 'zi_db';
     protected $table = 'sanggah_unit';
     protected $guarded = [
         'id'
     ];
 
+    
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+        ->logUnguarded('*');
+    }
     public function unitZI()
     {
         return $this->belongsTo(UnitZI::class, 'unit_zi_id');
