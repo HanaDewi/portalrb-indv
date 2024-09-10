79df7a72ca4337769faed5534ee78cb73f6b5b7f
Azkal Fikri <saklu07@gmail.com>
2024-09-10 09:54:20 +0700

    fix title

--- resources/views/webdashboard/rb-general-capaianoutput.blade.php
+++ resources/views/webdashboard/rb-general-capaianoutput.blade.php
@@ -12,7 +12,7 @@
                         <th rowspan="2" class="w-5">No.</th>
                         <th rowspan="2">Instansi Pemerintah</th>
                         <th rowspan="2">Group Instansi</th>
-                        <th colspan="4">Persentasi Realisasi</th>
+                        <th colspan="4">Tingkat Pengisian Output</th>
                         <th colspan="5">Rata-rata Persentasi Capaian Output</th>
                     </tr>
                     <tr>
