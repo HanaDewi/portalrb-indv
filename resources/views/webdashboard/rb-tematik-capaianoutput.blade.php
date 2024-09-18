79f2b9bab81c6e68f6bbe09751396c52b04e6d24
Azkal Fikri <saklu07@gmail.com>
2024-09-18 17:23:39 +0700

    fix

--- resources/views/webdashboard/rb-tematik-capaianoutput.blade.php
+++ resources/views/webdashboard/rb-tematik-capaianoutput.blade.php
@@ -13,7 +13,7 @@
                         <th rowspan="2">Instansi Pemerintah</th>
                         <th rowspan="2">Group Instansi</th>
                         <th colspan="4">Tingkat Pengisian Output</th>
-                        <th colspan="5">Rata-rata Persentasi Capaian Output</th>
+                        <th colspan="4">Rata-rata Persentasi Capaian Output</th>
                     </tr>
                     <tr>
                         <th>TW1</th><th>TW2</th><th>TW3</th><th>TW4</th>
