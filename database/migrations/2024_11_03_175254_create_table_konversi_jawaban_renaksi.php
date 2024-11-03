25704b51ef0319bc374841d8ff71c567268837eb
yopi <yopi@tura.group>
2024-11-04 06:27:33 +0700

    migration

---
+++ database/migrations/2024_11_03_175254_create_table_konversi_jawaban_renaksi.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('konversi_jawaban_renaksi', function (Blueprint $table) {
+            $table->id();
+            $table->string('jawaban', 10)->nullable();
+            $table->float('skor')->nullable();
+            $table->timestamps();
+        });
+        DB::table('konversi_jawaban_renaksi')->truncate();
+        DB::table('konversi_jawaban_renaksi')->insert(array(
+            [ 'jawaban'=> 'A', 'skor'=> 1.0 ],
+            [ 'jawaban'=> 'B', 'skor'=> 0.75 ],
+            [ 'jawaban'=> 'C', 'skor'=> 0.5 ],
+            [ 'jawaban'=> 'D', 'skor'=> 0.25 ],
+            [ 'jawaban'=> 'E', 'skor'=> 0.0 ],
+            [ 'jawaban'=> 'YA', 'skor'=> 1.0 ],
+            [ 'jawaban'=> 'TIDAK', 'skor'=> 0.0 ]
+        ));
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('konversi_jawaban_renaksi');
+    }
+};
