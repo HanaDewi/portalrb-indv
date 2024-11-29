87eb244334e54df0eb2fdeb78a38ef48f7264aa5
Oki Priyadi <oki.menpanrb@gmail.com>
2024-11-29 15:49:39 +0700

    update

--- app/Http/Controllers/ZI/EvaluatanController.php
+++ app/Http/Controllers/ZI/EvaluatanController.php
@@ -187,7 +187,7 @@ class EvaluatanController extends Controller
             $instansiZI = InstansiZI::where("id", $request->get("instansi_zi_id"))->first();
         }else{
             $instansiZI = InstansiZI::where("instansi_id", Auth::User()->user_rel->instansi->id)->first();
-            return redirect()->route('evaluatan_desk',$instansiZIid);
+            return redirect()->route('evaluatan_desk');
         }
 
         if($instansiZI){
