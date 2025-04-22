90bedb46c9141c4c97e87445a4771028bed1c268
Oki Priyadi <oki.menpanrb@gmail.com>
2025-04-22 16:14:27 +0700

    add home ZI

--- app/Http/Controllers/ZI/ZIController copy.php
+++ app/Http/Controllers/ZI/ZIController copy.php
@@ -21,18 +21,21 @@ class ZIController extends Controller
         });
     }
 
+
+
+
     public function index(Request $request)
     {
         if (Auth::User()->level == "tpn" || Auth::User()->level == "admin") {
             return redirect()->route('dashboard_zi');
         }
-        
-        if(!isset(Auth::User()->user_rel->instansi)){
+
+        if (!isset(Auth::User()->user_rel->instansi)) {
             abort('403');
         }
 
         $instansi_obj = Auth::User()->user_rel->instansi;
-        
+
         $instansi_id = $instansi_obj->id;
         $instansiZI = InstansiZI::where("instansi_id", $instansi_obj->id)->first();
 
@@ -42,9 +45,9 @@ class ZIController extends Controller
             return redirect('zi-administrasi');
         } elseif ($instansiZI->tahap_seleksi == 3) {
             return redirect()->route('evaluatan_hasil_sanggah');
-        }elseif($instansiZI->tahap_seleksi == 4){
+        } elseif ($instansiZI->tahap_seleksi == 4) {
             return redirect()->route('evaluatan_desk');
-        }elseif($instansiZI->tahap_seleksi == 5){
+        } elseif ($instansiZI->tahap_seleksi == 5) {
             return redirect()->route('evaluatan_hasil_akhir');
         }
 
