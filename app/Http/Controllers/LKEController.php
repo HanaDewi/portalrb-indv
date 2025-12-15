53fbc0861cbd128ea58dcf28e0952553429e17da
Azkal Fikri <saklu07@gmail.com>
2025-12-15 07:03:00 +0700

    fix capaian output dan coi

--- app/Http/Controllers/LKEController.php
+++ app/Http/Controllers/LKEController.php
@@ -576,7 +576,6 @@ class LKEController extends Controller
             })
             ->orderBy('lke_parameter_id')
             ->get();
-            dd($instansi->group, $parameters);
         $nationalStats = collect();
         if ($parameters->isNotEmpty()) {
             $nationalStats = LkeTestTpLine::whereIn('lke_bobot_id', $parameters->pluck('id'))
