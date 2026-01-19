05db28b4d733fdcc26eeaf4ea41722a6410bf444
Azkal Fikri <saklu07@gmail.com>
2026-01-19 18:48:16 +0700

    force css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
         <meta name="author" content="MENPANRB">
         <title>KEMENPANRB - LKE RB</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-        <link rel="stylesheet" href="{{ asset('template_lkerb') }}/dist/css/app.css" />
+        <link rel="stylesheet" href="{{ asset('template_lkerb') }}/dist/css/app.css?{{ date('his') }}" />
         <style>
             .password-container {
                 position: relative;
