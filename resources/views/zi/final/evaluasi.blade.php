30db000e819e4dcf20bf10846f5a66345b4a7011
Oki Priyadi <oki.menpanrb@gmail.com>
2026-02-04 21:21:44 +0700

    fitur tambah link  penghargaan di ZI

--- resources/views/zi/final/evaluasi.blade.php
+++ resources/views/zi/final/evaluasi.blade.php
@@ -114,14 +114,29 @@
                         </a>
                     </div>
                 </div>
-
-
                 <hr />
-
-
                 <br />
-                <br /><br />
-
+                <div class="col-span-12 grid grid-cols-12 gap-6">
+                    <div class="col-span-12 ">
+                        <button onclick="tambah_penghargaan({{ $instansi_ZI->id }});"
+                            class="btn btn-primary btn-sm kirim-file"><i data-lucide="plus" class="w-4 h-4 mr-1"></i>
+                            Tambah Penghargaan</button><br>
+                    </div>
+                    <div class="col-span-12 ">
+                        @if(isset($penghargaans))
+                        <ol style="list-style:decimal">
+                            @foreach($penghargaans as $penghargaan)
+                            <li style="margin-left: 20px; padding-left:10px; font-size:1.2em; "> {{
+                                $penghargaan->keterangan }} :
+                                <a href="{{$penghargaan->link}}" target="_blank">{{$penghargaan->link}}</a>
+                            </li>
+                            @endforeach
+                        </ol>
+                        @endif
+                    </div>
+                </div>
+                <br />
+                <br />
             </div>
 
 
@@ -448,6 +463,51 @@
 </div>
 <!-- END: Modal Content -->
 
+
+{{-- Modal Penghargaan --}}
+<div id="modal_penghargaan" class="modal fade" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <!-- BEGIN: Modal Header -->
+            <div class="darkbg modal-header">
+                <h2 class="font-bold fw-medium fs-base me-auto">Tambah Penghargaan</h2>
+            </div> <!-- END: Modal Header -->
+            <!-- BEGIN: Modal Body -->
+            <form action="{{ route('proses_tambah_penghargaan_simpan') }}" method="post" enctype="multipart/form-data">
+                @csrf
+                <input type="hidden" name="instansi_id" value="{{$instansi_ZI->id}}">
+                <div class="modal-body grid columns-12 gap-4 gap-y-3">
+                    <div class="g-col-12">
+                        <table class="table">
+                            <tr>
+                                <td class="font-bold w-44">Link Penghargaan <span class="text-danger">*</span></td>
+                                <td>
+                                    <input type="text" class="form-control" id="link_penghargaan"
+                                        name="link_penghargaan"
+                                        placeholder="inputkan link drive ke dokumen penghargaan">
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="font-bold w-44">Keterangan <span class="text-danger">*</span></td>
+                                <td>
+                                    <input type="text" class="form-control" id="deskripsi_penghargaan"
+                                        name="deskripsi_penghargaan" placeholder="inputkan keterangan">
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div> <!-- END: Modal Body -->
+                <!-- BEGIN: Modal Footer -->
+                <div class="modal-footer text-end">
+                    <button type="button" data-tw-dismiss="modal"
+                        class="btn btn-outline-secondary w-20 me-1">Batal</button>
+                    <button type="submit" class="btn btn-primary w-20 saveButton">Simpan</button>
+                </div> <!-- END: Modal Footer -->
+            </form>
+        </div>
+    </div>
+</div>
+<!-- END: Modal Content -->
 @endsection
 
 @push('css')
@@ -469,20 +529,14 @@
     var idx = {{ $instansi_ZI->id }};
     function upload_lhe(id) {
         modal_upload_lhe.show();
-                // $.getJSON("{{ url('hasil/get_test_tp') }}/" + id, function(data) {
-                //     $('#bobot_rb_general_penyesuaian').val(data.bobot_rb_general_penyesuaian);
-                //     $('#berkas_list').html(data.berkas_list);
-                //     $('.saveButton').prop('disabled', false);
-                // });
     }
 
     function upload_undangan(id) {
         modal_upload_undangan.show();
-                // $.getJSON("{{ url('hasil/get_test_tp') }}/" + id, function(data) {
-                //     $('#bobot_rb_general_penyesuaian').val(data.bobot_rb_general_penyesuaian);
-                //     $('#berkas_list').html(data.berkas_list);
-                //     $('.saveButton').prop('disabled', false);
-                // });
+    }
+
+    function tambah_penghargaan(id) {
+        modal_penghargaan.show();
     }
 
     function pilih_berkas() {
@@ -501,6 +555,7 @@
         cek_berkas_undangan(this);
     })
 
+    
     function isAllowed(ext) {
         switch (ext.toLowerCase()) {
             //case 'xlsx':
@@ -595,7 +650,7 @@
 
         modal_upload_lhe = tailwind.Modal.getInstance(document.querySelector("#modal-upload-lhe"));
         modal_upload_undangan = tailwind.Modal.getInstance(document.querySelector("#modal_upload_undangan"));
-
+        modal_penghargaan = tailwind.Modal.getInstance(document.querySelector("#modal_penghargaan"));
         $('.openNew').click(function(event) {
             event.preventDefault();
             window.open(
